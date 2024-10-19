02e226f5f419440ba2032ea8179725dc44846da9
MinPau20 <paula.alvarado0820@gmail.com>
2024-10-18 21:55:58 -0500

    Se hacen las importanciones se  enruta y organizan las carpetas del historial

--- front-proyecto/src/App.jsx
+++ front-proyecto/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Ingresoregistro from './components/ingresoregistro/Ingresoregistro'
+import Historial from './components/historial/Historial'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Ingresoregistro/>} />
+        <Route path="/historial" element={<Historial/>} />
       </Routes>
     </Router>
   )
---
+++ front-proyecto/src/components/historial/Historial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Historial.css'
+import { useNavigate } from 'react-router-dom';
+
+function Historial(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default Historial
