8b51d3dab81139806b24f14d1a05b0b91fe2f03b
ZhantyDev <unpajarocantor07@gmail.com>
2024-11-04 14:19:31 -0500

    se arregla la funcion de historial

--- back-proyecto/src/controllers/histotrial.controller.js
+++ back-proyecto/src/controllers/histotrial.controller.js
@@ -1,9 +1,20 @@
-import { getConnection } from "../database/database.js";
+import { getConnection } from '../database/database.js';
 
-export const historial = async(req, res)=>{
-    let connection
-    const { cuenta_id } = req.body
-    connection = await getConnection()
-    const info = await connection.query('select * from Reportes where cuenta_id = ?', [cuenta_id])
-    return res.json(info)
-}
+export const historial = async (req,res) => {
+    let connection;
+    const {cuenta_id} = req.body    
+    try {
+        connection = await getConnection();
+    const [info] = await connection.query('SELECT * FROM Reportes WHERE cuenta_id = ?', [cuenta_id]);
+        
+        if (info.length === 0) {
+            console.log('No se encontraron reportes');
+            return res.status(401).json({ message: "no se encontraron reportes" });
+        } else {
+            console.log('Información encontrada:', info); // Muestra el resultado en consola
+            return res.status(200).json({ message: "Inicio de sesión exitoso", info });
+        }
+    } catch (error) {
+        res('Error al obtener el historial:', error);
+    } 
+};
