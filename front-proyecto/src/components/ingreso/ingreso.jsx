31934a84ffee9fe14c908dbeccbbbbe3c3a2c02b
MinPau20 <paula.alvarado0820@gmail.com>
2024-11-03 23:36:56 -0500

    Se conecta el front con el back para permitir el funcionamiento de la interfaz de inicio de sesión

--- front-proyecto/src/components/ingreso/ingreso.jsx
+++ front-proyecto/src/components/ingreso/ingreso.jsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import './Ingreso.css'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 
 function Ingresoregistro(){
     const navigate = useNavigate()
+    const [form, setForm] = useState({ email: '', contraseña: '' });
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/api", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(form)
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                alert(data.message);
+                navigate('/home');
+            } else {
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error("Error en el inicio de sesión:", error);
+        }
+    };
+
     return(
         <>
         <div id = "cuadro">
-            <div class = "cuadrito" >
-                <h1 class = "letra" >Inicio de sesion</h1>
-                <input type="text" name='input_usuario' class = 'item' placeholder='Usuario' />
-                <input type="password" name='input_contraseña' class = 'item' placeholder='Contraseña' />
-                <div class = "div">
-                    <button class = 'botonStyle' onClick={()=>navigate('/home')}>Iniciar</button>
-                    <button class = 'botonStyle' onClick={()=>navigate('/registro')}>Registro</button>
+            <div className = "cuadrito" >
+                <h1 className = "letra" >Inicio de sesion</h1>
+                <input type="text" name='email' className = 'item' placeholder='Usuario' onChange={handleChange}/>
+                <input type="password" name='contraseña' className = 'item' placeholder='Contraseña' onChange={handleChange}/>
+                <div className = "div">
+                    <button className = 'botonStyle' onClick={handleSubmit}>Iniciar</button>
+                    <button className = 'botonStyle' onClick={()=>navigate('/registro')}>Registro</button>
                 </div>
             </div>
         </div>
