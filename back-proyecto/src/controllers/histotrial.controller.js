839d15755177b340aab84c54ada614083f3b8dfd
ZhantyDev <unpajarocantor07@gmail.com>
2024-11-04 14:48:15 -0500

    se arregla el error del controlador de historial

--- back-proyecto/src/controllers/histotrial.controller.js
+++ back-proyecto/src/controllers/histotrial.controller.js
@@ -1,20 +1,22 @@
 import { getConnection } from '../database/database.js';
 
-export const historial = async (req,res) => {
+export const historial = async (req, res) => {
     let connection;
-    const {cuenta_id} = req.body    
+    const { cuenta_id } = req.body; // Extrae `cuenta_id` de `req.body`
+
     try {
         connection = await getConnection();
-    const [info] = await connection.query('SELECT * FROM Reportes WHERE cuenta_id = ?', [cuenta_id]);
-        
+        const [info] = await connection.query('SELECT * FROM Transacciones WHERE cuenta_id = ?', [cuenta_id]);
+
         if (info.length === 0) {
             console.log('No se encontraron reportes');
-            return res.status(401).json({ message: "no se encontraron reportes" });
+            return res.status(401).json({ message: "No se encontraron reportes" });
         } else {
-            console.log('Información encontrada:', info); // Muestra el resultado en consola
+            console.log('Información encontrada:', info);
             return res.status(200).json({ message: "Inicio de sesión exitoso", info });
         }
     } catch (error) {
-        res('Error al obtener el historial:', error);
-    } 
+        console.error('Error al obtener el historial:', error);
+        return res.status(500).json({ message: 'Error al obtener el historial', error });
+    }
 };
